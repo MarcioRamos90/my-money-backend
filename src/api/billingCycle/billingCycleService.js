266336a47ccae2e50e0974223830e9389c2ae7c9
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-07-19 16:25:12 -0300

    criando rota /count

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingoCycle = require("./billingCycle");
 BillingoCycle.methods(["get", "post", "put", "delete"]);
 BillingoCycle.updateOptions({ new: true, runValidators: true });
 
+BillingoCycle.route("count", (req, res, next) => {
+  BillingoCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] });
+    } else {
+      res.json({ value });
+    }
+  });
+});
+
 module.exports = BillingoCycle;
