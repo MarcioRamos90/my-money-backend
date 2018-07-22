31c57fd2be2943fd9610c94a30f432e6ed201bec
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-07-22 18:27:02 -0300

    Criando autenticação no projeto

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const auth = require("./auth");
 
 module.exports = function(server) {
-  // Definir URL base para todas as rotas
-  const router = express.Router();
-  server.use("/api", router);
+  // Rotas protegidas por Token JWT
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
 
-  // Rotas do ciclo de pagamento
+  protectedApi.use(auth);
+
   const BillingCycle = require("../api/billingCycle/billingCycleService");
-  BillingCycle.register(router, "/billingCycles");
+  BillingCycle.register(protectedApi, "/billingCycle");
+
+  // Rotas abertas
+  const openApi = express.Router();
+  server.use("oapi", openApi);
+
+  const AuthService = require("../api/user/authService");
+  openApi.post("login", AuthService.login);
+  openApi.post("signup", AuthService.signup);
+  openApi.post("validateToken", AuthService.validateToken);
 };
